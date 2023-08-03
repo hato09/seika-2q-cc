b0ff1c7119fe07e3ce7a680df910dc9222e81c56
hato09 <i221t013@st.kyoto-seika.ac.jp>
2023-08-03 15:24:56 +0900

    行動（ダンス）追加、その他修正を行なった

--- src/app.js
+++ src/app.js
@@ -4,34 +4,40 @@ import anime from 'animejs/lib/anime.es.js';
 let count= 1;
 let iti=0;
 function myEventHandler(event){
-	event.currentTarget.textContent = "ボタン：" + count;
+	event.currentTarget.textContent = "移動 押した回数" + count;
 	if(iti<600){
 		anime({
 			targets: 'div',
 			translateX:iti,
-			// scale:2,
-			rotate: '1turn',
+			rotate: '360deg',
 			// backgroundColor: '#FFF',
 			duration: 3000,
-			// complete:function(anime){
-			// 	rotate+ = '1turn'
-			// },
 	    	});
 		count++;
 		iti+=150;
 		console.log(iti);
 	}else{
+		iti -=500;
 		anime({
 			targets:'div',
 			translateX:iti,
 			// scale:2,
-			rotate: '1turn',
+			rotate: '360deg',
 			// backgroundColor: '#FFF',
 			duration: 3000,
 		})
 		count++;
-		iti -=500;
 		console.log(iti);
+	}
+	if(count>10){
+		event.currentTarget.textContent = "ダンス　押した回数" + count;
+		anime({
+			targets:'div',
+			translateX: iti,
+			direction: 'alternate',
+			easing: 'steps(5)'
+		})
+
 	}
 }
 let button = document.getElementById("my-button");
