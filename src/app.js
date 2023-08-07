b15eb15f2c914763b4bc48e93c9fd3d9339cceda
hato09 <i221t013@st.kyoto-seika.ac.jp>
2023-08-08 01:06:54 +0900

    ゴールからゴールキーパーに変更、最終調整をした

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import anime from 'animejs/lib/anime.es.js';
 
 
 let count= 1;
+let count2= 11;
 let iti=0;
+let kaiten =0;
 function myEventHandler(event){
-	event.currentTarget.textContent = "移動 押した回数" + count;
+	event.currentTarget.textContent = "回転移動 押した回数：" + count;
 	if(iti<600){
+		kaiten +=360;
 		anime({
 			targets: 'div',
 			translateX:iti,
-			rotate: '360deg',
+			rotate: kaiten + 'deg',
 			// backgroundColor: '#FFF',
 			duration: 3000,
 	    	});
@@ -21,24 +24,26 @@ function myEventHandler(event){
 		anime({
 			targets:'div',
 			translateX:iti,
-			// scale:2,
-			rotate: '360deg',
+			rotate: kaiten + 'deg',
 			// backgroundColor: '#FFF',
 			duration: 3000,
-		})
+		});
 		count++;
 		console.log(iti);
+		console.log(count2);
 	}
-	if(count>10){
-		event.currentTarget.textContent = "ダンス　押した回数" + count;
+
+	if(count>11){
+		event.currentTarget.textContent = "ダンス　押した回数：" + count2;
 		anime({
 			targets:'div',
 			translateX: iti,
 			direction: 'alternate',
-			easing: 'steps(5)'
-		})
-
+			easing: 'steps(5)',
+		});
+		count2++;
 	}
+	console.log(count2);
 }
 let button = document.getElementById("my-button");
 button.onclick = myEventHandler;
